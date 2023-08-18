f0de65f88258c4b77e1ec1a2490dec027d9f7c98
kurt mcgoona <kurtm36@gmail.com>
2023-08-18 18:42:02 +0100

    Add Btc Dom and TotalMC data

--- api/coincap_global.py
+++ api/coincap_global.py
@@ -1,5 +1,12 @@
+"""
+This file is strictly for API code 
+"""
+
 import requests    
 import json
+
+local_currency = 'EUR'
+local_symbol = '€'
 
 """
 API Key varible. Generated @ https://pro.coinmarketcap.com/account . Login Creds will be in README if required.
@@ -8,10 +15,11 @@ API_KEY = "ae55d981-0a3f-4fab-82d8-bfd27701bda3"
 headers = {"X-CMC_PRO_API_KEY": API_KEY}
 
 BASE_URL = 'https://pro-api.coinmarketcap.com'
-GLOBAL_URL = BASE_URL + '/v1/global-metrics/quotes/latest'
+GLOBAL_URL = BASE_URL + '/v1/global-metrics/quotes/latest?convert=' + local_currency
 
 request = requests.get(GLOBAL_URL, headers=headers)
 results = request.json()
 
 print(json.dumps(results, sort_keys=True, indent=4))
 
+data = results["data"]
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import requests    
 import json
 
+local_currency = 'EUR'
+local_symbol = '€'
+
 """
 API Key varible. Generated @ https://pro.coinmarketcap.com/account . Login Creds will be in README if required.
 """
@@ -8,9 +11,25 @@ API_KEY = "ae55d981-0a3f-4fab-82d8-bfd27701bda3"
 headers = {"X-CMC_PRO_API_KEY": API_KEY}
 
 BASE_URL = 'https://pro-api.coinmarketcap.com'
-GLOBAL_URL = BASE_URL + '/v1/global-metrics/quotes/latest'
+GLOBAL_URL = BASE_URL + '/v1/global-metrics/quotes/latest?convert=' + local_currency
 
 request = requests.get(GLOBAL_URL, headers=headers)
 results = request.json()
 
 print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results["data"]
+
+
+"""
+Total Market Cap
+"""
+total_market_cap = data["quote"][local_currency]["total_market_cap"]
+print(f"Total Market Cap : {total_market_cap}")
+
+
+"""
+Bitcoin dominance % (BTC Market cap)
+"""
+btc_dominance = data["btc_dominance"]
+print(f"BitCoin Dominance : {btc_dominance}")
