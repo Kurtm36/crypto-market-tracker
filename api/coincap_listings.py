c7d9c62ab98940ac2ca8914ecd5df61963d1390a
kurt mcgoona <kurtm36@gmail.com>
2023-08-19 21:50:53 +0100

    Format and print data to terminal for readability

--- api/coincap_listings.py
+++ api/coincap_listings.py
@@ -16,17 +16,34 @@ GLOBAL_URL = BASE_URL + '/v1/cryptocurrency/listings/latest?convert=' + local_cu
 request = requests.get(GLOBAL_URL, headers=headers)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+#print(json.dumps(results, sort_keys=True, indent=4))
 data = results["data"]
 
 for currency in data : 
     name = currency["name"]
     symbol = currency["symbol"]
-
+    #Pulling data varibles
     price = currency["quote"][local_currency]["price"]
     percent_change_24hr = currency["quote"][local_currency]["percent_change_24h"]
     percent_change_7day = currency["quote"][local_currency]["percent_change_7d"]
     percent_change_30day = currency["quote"][local_currency]["percent_change_30d"]
     market_cap = currency["quote"][local_currency]["market_cap"]
-    
-    
+    #Rounding data
+    price = round(price , 2)
+    percent_change_24hr = round(percent_change_24hr, 2)
+    percent_change_7day = round(percent_change_7day, 2)
+    percent_change_30day = round(percent_change_30day, 2)
+    market_cap = round(market_cap, 2)
+    #Formatting data
+    string_price = local_symbol + "{:,}".format(price)
+    string_percent_change_24hr = local_currency + "{:,}".format(percent_change_24hr)
+    string_percent_change_7day = local_currency + "{:,}".format(percent_change_7day)
+    string_percent_change_30day = local_currency + "{:,}".format(percent_change_30day)
+    string_market_cap = local_currency + "{:,}".format(market_cap)
+    #Printing the data
+    print(name + " (" + symbol +")")
+    print("Price : " + string_price)
+    print("24hr Change : " + string_percent_change_24hr)
+    print("7 Day Change : " + string_percent_change_7day)
+    print("3 Day Change : " + string_percent_change_30day)
+    print("Market Cap : " + string_market_cap)
