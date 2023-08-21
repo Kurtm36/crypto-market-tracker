c08c287d1f48da8b2f69863f9ee5be75140651ad
kurt mcgoona <kurtm36@gmail.com>
2023-08-21 21:59:57 +0100

    complete quotes file and func, incorperated to menu

--- api/coincap_quotes_func.py
+++ api/coincap_quotes_func.py
@@ -12,41 +12,43 @@ headers = {"X-CMC_PRO_API_KEY": API_KEY}
 
 BASE_URL = 'https://pro-api.coinmarketcap.com'
 
-symbol = input("Enter the ticker symbol of the cryptocurrency : ")
+symbol = input("Enter the ticker symbol of the cryptocurrency(CAP SENSITIVE I.E BTC , XRP , ETH) : ")
 
 GLOBAL_URL = BASE_URL + '/v1/cryptocurrency/quotes/latest?convert=' + local_currency + "&symbol=" + symbol
 
 request = requests.get(GLOBAL_URL, headers=headers)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+#print(json.dumps(results, sort_keys=True, indent=4))
 data = results["data"]
+currency = data[symbol]
+name = currency["name"]
 
-# for currency in data : 
-#     name = currency["name"]
-#     symbol = currency["symbol"]
-#     #Pulling data varibles
-#     price = currency["quote"][local_currency]["price"]
-#     percent_change_24hr = currency["quote"][local_currency]["percent_change_24h"]
-#     percent_change_7day = currency["quote"][local_currency]["percent_change_7d"]
-#     percent_change_30day = currency["quote"][local_currency]["percent_change_30d"]
-#     market_cap = currency["quote"][local_currency]["market_cap"]
-#     #Rounding data
-#     price = round(price , 2)
-#     percent_change_24hr = round(percent_change_24hr, 2)
-#     percent_change_7day = round(percent_change_7day, 2)
-#     percent_change_30day = round(percent_change_30day, 2)
-#     market_cap = round(market_cap, 2)
-#     #Formatting data
-#     string_price = local_symbol + "{:,}".format(price)
-#     string_percent_change_24hr = local_currency + "{:,}".format(percent_change_24hr)
-#     string_percent_change_7day = local_currency + "{:,}".format(percent_change_7day)
-#     string_percent_change_30day = local_currency + "{:,}".format(percent_change_30day)
-#     string_market_cap = local_currency + "{:,}".format(market_cap)
-#     #Printing the data
-#     print(name + " (" + symbol +")")
-#     print("Price : " + string_price)
-#     print("24hr Change : " + string_percent_change_24hr)
-#     print("7 Day Change : " + string_percent_change_7day)
-#     print("3 Day Change : " + string_percent_change_30day)
-#     print("Market Cap : " + string_market_cap + "\n")
+#Pulling data varibles
+price = currency["quote"][local_currency]["price"]
+percent_change_24hr = currency["quote"][local_currency]["percent_change_24h"]
+percent_change_7day = currency["quote"][local_currency]["percent_change_7d"]
+percent_change_30day = currency["quote"][local_currency]["percent_change_30d"]
+market_cap = currency["quote"][local_currency]["market_cap"]
+
+#Rounding data
+price = round(price , 2)
+percent_change_24hr = round(percent_change_24hr, 2)
+percent_change_7day = round(percent_change_7day, 2)
+percent_change_30day = round(percent_change_30day, 2)
+market_cap = round(market_cap, 2)
+
+#Formatting data
+string_price = local_symbol + "{:,}".format(price)
+string_percent_change_24hr = local_currency + "{:,}".format(percent_change_24hr)
+string_percent_change_7day = local_currency + "{:,}".format(percent_change_7day)
+string_percent_change_30day = local_currency + "{:,}".format(percent_change_30day)
+string_market_cap = local_currency + "{:,}".format(market_cap)
+
+#Printing the data
+print(name + " (" + symbol +")")
+print("Price : " + string_price)
+print("24hr Change : " + string_percent_change_24hr)
+print("7 Day Change : " + string_percent_change_7day)
+print("3 Day Change : " + string_percent_change_30day)
+print("Market Cap : " + string_market_cap + "\n")
--- run.py
+++ run.py
@@ -96,48 +96,53 @@ def coincap_coin_listings(local_currency, local_symbol):
         print("Market Cap : " + string_market_cap + "\n")
 execute_coincap_listings = True
 
-# Cypto Quotes (Enter ticker symbol)
+# Cypto Quotes (Enter ticker symbol (CAPS ONLY))
 def coincap_quotes():
 
-    symbol = input("Enter the ticker symbol of a cryptocurrency")
-
     BASE_URL = 'https://pro-api.coinmarketcap.com'
+
+    symbol = input("Enter the ticker symbol of the cryptocurrency(CAP SENSITIVE I.E BTC , XRP , ETH) : ")
+
     GLOBAL_URL = BASE_URL + '/v1/cryptocurrency/quotes/latest?convert=' + local_currency + "&symbol=" + symbol
 
     request = requests.get(GLOBAL_URL, headers=headers)
     results = request.json()
 
-    #print(json.dumps(results, sort_keys=True, indent=4))
+    #Basic coin infomation
     data = results["data"]
+    currency = data[symbol]
+    name = currency["name"]
 
-    for currency in data : 
-        name = currency["name"]
-        symbol = currency["symbol"]
-        #Pulling data varibles
-        price = currency["quote"][local_currency]["price"]
-        percent_change_24hr = currency["quote"][local_currency]["percent_change_24h"]
-        percent_change_7day = currency["quote"][local_currency]["percent_change_7d"]
-        percent_change_30day = currency["quote"][local_currency]["percent_change_30d"]
-        market_cap = currency["quote"][local_currency]["market_cap"]
-        #Rounding data
-        price = round(price , 2)
-        percent_change_24hr = round(percent_change_24hr, 2)
-        percent_change_7day = round(percent_change_7day, 2)
-        percent_change_30day = round(percent_change_30day, 2)
-        market_cap = round(market_cap, 2)
-        #Formatting data
-        string_price = local_symbol + "{:,}".format(price)
-        string_percent_change_24hr = local_currency + "{:,}".format(percent_change_24hr)
-        string_percent_change_7day = local_currency + "{:,}".format(percent_change_7day)
-        string_percent_change_30day = local_currency + "{:,}".format(percent_change_30day)
-        string_market_cap = local_currency + "{:,}".format(market_cap)
-        #Printing the data
-        print(name + " (" + symbol +")")
-        print("Price : " + string_price)
-        print("24hr Change : " + string_percent_change_24hr)
-        print("7 Day Change : " + string_percent_change_7day)
-        print("3 Day Change : " + string_percent_change_30day)
-        print("Market Cap : " + string_market_cap + "\n")
+    #Pulling data varibles
+    price = currency["quote"][local_currency]["price"]
+    percent_change_24hr = currency["quote"][local_currency]["percent_change_24h"]
+    percent_change_7day = currency["quote"][local_currency]["percent_change_7d"]
+    percent_change_30day = currency["quote"][local_currency]["percent_change_30d"]
+    market_cap = currency["quote"][local_currency]["market_cap"]
+
+    #Rounding data
+    price = round(price , 2)
+    percent_change_24hr = round(percent_change_24hr, 2)
+    percent_change_7day = round(percent_change_7day, 2)
+    percent_change_30day = round(percent_change_30day, 2)
+    market_cap = round(market_cap, 2)
+
+    #Formatting data
+    string_price = local_symbol + "{:,}".format(price)
+    string_percent_change_24hr = local_currency + "{:,}".format(percent_change_24hr)
+    string_percent_change_7day = local_currency + "{:,}".format(percent_change_7day)
+    string_percent_change_30day = local_currency + "{:,}".format(percent_change_30day)
+    string_market_cap = local_currency + "{:,}".format(market_cap)
+
+    #Printing the data
+    print(name + " (" + symbol +")")
+    print("Price : " + string_price)
+    print("24hr Change : " + string_percent_change_24hr)
+    print("7 Day Change : " + string_percent_change_7day)
+    print("3 Day Change : " + string_percent_change_30day)
+    print("Market Cap : " + string_market_cap + "\n")
+execute_coincap_quotes = True
+
 # Clear text for function for terminal 
 def clear_text_terminal():
     os.system("cls" if os.name =="nt" else "clear")
@@ -146,12 +151,13 @@ def clear_text_terminal():
 def display_Menu():
     print("Kurts Cypto Data Center")
     print("Select 1 for Global Market Data")
-    print("Select 2 for Global listings Data ")
-    print("Select 3 for Global Market Data")
+    print("Select 2 for Coin Listings Data ")
+    print("Select 3 for Coin Quotes ")
     print("Select 4 for Global Market Data")
     print("Select 5 for Global Market Data")
     print("Select 0 to Exit")
 
+# Menu while loop
 while True:
     clear_text_terminal()   
     display_Menu()
@@ -173,8 +179,10 @@ while True:
         execute_coincap_listings = True
     elif user_input == 3:
         clear_text_terminal()   
-        print("you have selected [3]")
+        coincap_quotes()
+        execute_coincap_quotes = False
         input("Press [Enter] to return to Menu")
+        execute_coincap_quotes = True
     elif user_input == 4:
         clear_text_terminal()   
         print("you have selected [4]")
