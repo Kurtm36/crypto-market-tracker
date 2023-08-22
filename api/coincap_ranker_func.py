823e5f9c201fc2e97d55661a1d23716a2611f3dd
kurt mcgoona <kurtm36@gmail.com>
2023-08-22 20:11:49 +0100

    Enhance display of table with color indicators

--- api/coincap_ranker_func.py
+++ api/coincap_ranker_func.py
@@ -13,29 +13,29 @@ headers = {"X-CMC_PRO_API_KEY": API_KEY}
 BASE_URL = 'https://pro-api.coinmarketcap.com'
 
 # Menu for Ranking Function
+
 print()
 print("CoinMarketCap Explorer Menu")
 print()
-print("[1]- Top 100 sorted by market cap")
-print("[2]- Top 100 sorted by 24hr percent change")
-print("[3]- Top 100 sorted by 24hr trading volume")
+print("[1]- Top 100 Sorted By Market cap")
+print("[2]- Top 100 Sorted By 24hr Trading Volume ")
+print("[3]- Top 100 Sorted By 24hr Percent Change")
+print("[4]- Top 100 Sorted By 7 day Change")
+print("[5]- Top 100 Sorted By 30 day Change")
 print("[0]- Exit")
 
-choice = input("What information would you like to view? (1-3)")
-
-sort = ""
+choice = input("What information would you like to view? (1-3 or 0 to exit) :  ")
 
 if choice == "1":
     sort = "market_cap"
 elif choice == "2":
-    sort = "percent_change_24h"
-elif choice == "3":
     sort = "volume_24h"
+elif choice == "3":
+    sort = "percent_change_24h"
 elif choice == "0":
     exit(0)
 else:
-    print("Invalid choice")
-    exit(1)
+    print("Invalid choice. Please select a valid option.")
 
 
 quote_url = BASE_URL + "/v1/cryptocurrency/listings/latest?convert=" + local_currency + "&sort=" + sort
@@ -65,27 +65,43 @@ for currency in data:
 
     price = quote["price"]
     volume = quote["volume_24h"]
-    # Formatting data    
+    # Formatting data with Color Indicators    
     if percent_change_1hr is not None:
         percent_change_1hr = round(percent_change_1hr, 2)
+        if percent_change_1hr > 0:
+            percent_change_1hr = Back.GREEN + str(percent_change_1hr) + "%" + Style.RESET_ALL
+        else:
+            percent_change_1hr = Back.RED + str(percent_change_1hr) + "%" + Style.RESET_ALL    
         
     if percent_change_24hr is not None:
         percent_change_24hr = round(percent_change_24hr, 2)
+        if percent_change_24hr > 0:
+            percent_change_24hr = Back.GREEN + str(percent_change_24hr) + "%" + Style.RESET_ALL
+        else:
+            percent_change_24hr = Back.RED + str(percent_change_24hr) + "%" + Style.RESET_ALL
 
     if percent_change_7d is not None:
         percent_change_7d = round(percent_change_7d, 2)
+        if percent_change_7d > 0:
+            percent_change_7d = Back.GREEN + str(percent_change_7d) + "%" + Style.RESET_ALL
+        else:
+            percent_change_7d = Back.RED + str(percent_change_7d) + "%" + Style.RESET_ALL
 
     if percent_change_30d is not None:
-        percent_change_30d = round(percent_change_30d, 2)            
+        percent_change_30d = round(percent_change_30d, 2)
+        if percent_change_30d > 0:
+            percent_change_30d = Back.GREEN + str(percent_change_30d) + "%" + Style.RESET_ALL
+        else:
+            percent_change_30d = Back.RED + str(percent_change_30d) + "%" + Style.RESET_ALL            
 
     if volume is not None:
-        volume_string = "{:,}".format(round(price, 2))
+        volume_string = "{:,}".format(round(volume, 2))
 
     if market_cap is not None:
-        market_cap_string = "{:,}".format(round(price, 2))
+        market_cap_string = "{:,}".format(round(market_cap, 2))
 
     price_string = "{:,}".format(round(price, 2)) 
-
+    # Table formatting 
     table.add_row([name + " (" + symbol + ')',
                    local_symbol + price_string,
                    local_symbol + market_cap_string,
@@ -94,6 +110,8 @@ for currency in data:
                    str(percent_change_24hr),
                    str(percent_change_7d),
                    str(percent_change_30d),]) 
+
+
 print()
 print(table)
 print()           
